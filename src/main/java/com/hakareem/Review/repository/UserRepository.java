43d28cfee642a4c49214c466706d89bcd0787fed
hakareem <harithakareem72@gmail.com>
2023-03-30 11:38:16 +0100

    user repo

---
+++ src/main/java/com/hakareem/Review/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.hakareem.Review.repository;
+
+import com.hakareem.Review.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}

677f1741d52c706dd6e8542127c03a0b4e70ce00
hakareem <harithakareem72@gmail.com>
2023-03-30 11:42:00 +0100

    load user by username

--- src/main/java/com/hakareem/Review/service/UserDetailServiceImpl.java
+++ src/main/java/com/hakareem/Review/service/UserDetailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hakareem.Review.service;
 
 import com.hakareem.Review.domain.User;
+import com.hakareem.Review.repository.UserRepository;
 import com.hakareem.Review.util.CustomPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,18 +9,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
 
     @Autowired
-    private CustomPasswordEncoder customPasswordEncoder;
+    private UserRepository userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(customPasswordEncoder.getPasswordEncoder().encode("asdfasdf"));
-        user.setId(1L);
-        return user;
+        Optional<User> userOpt = userRepo.findByUsername(username);
+        return userOpt.orElseThrow(() -> new UsernameNotFoundException("Invalid Credentials"));
     }
 }

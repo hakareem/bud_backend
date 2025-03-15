a789c2a859a24574ce1d5fabcec3f2b46fcfa79b
Harith A Kareem <harithakareem72@gmail.com>
2025-03-15 15:27:14 +0000

    Add methods to AssignmentService for assignment retrieval

--- src/main/java/com/hakareem/Review/service/AssignmentService.java
+++ src/main/java/com/hakareem/Review/service/AssignmentService.java
@@ -3,6 +3,12 @@ package com.hakareem.Review.service;
 import com.hakareem.Review.domain.Assignment;
 import com.hakareem.Review.domain.User;
 import com.hakareem.Review.repository.AssignmentRepository;
+
+import java.lang.StackWalker.Option;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +24,24 @@ public class AssignmentService {
         assignment.setUser(user);
         return assignmentRepository.save(assignment);
     };
+
+    public List<Assignment> getAssignments(User user) {
+        return assignmentRepository.findAllByUserId(user.getId());
+    }
+
+    public Set<Assignment> getAssignment(User user) {
+        return assignmentRepository.findByUser(user);
+    }
+
+    public Assignment getAssignmentByIdAndUser(Long id, User user) {
+        return assignmentRepository.findByIdAndUser(id, user);
+    }
+
+   public Optional<Assignment> findById(Long assignmentId) {
+        return assignmentRepository.findById(assignmentId);
+    }
+
+    public Assignment save(Assignment assignment) {
+       return assignmentRepository.save(assignment);
+    }
 }

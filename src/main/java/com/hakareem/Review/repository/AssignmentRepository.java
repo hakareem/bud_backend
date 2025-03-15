24c650271a73d751443f8ba97589eab60a42fd18
Harith A Kareem <harithakareem72@gmail.com>
2025-03-15 15:28:03 +0000

    updated AssignmentRepository

--- src/main/java/com/hakareem/Review/repository/AssignmentRepository.java
+++ src/main/java/com/hakareem/Review/repository/AssignmentRepository.java
@@ -1,9 +1,20 @@
 package com.hakareem.Review.repository;
 
 import com.hakareem.Review.domain.Assignment;
+import com.hakareem.Review.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+    Assignment findByUserId(Long userId);
+    List<Assignment> findAllByUserId(Long userId);
+    Set<Assignment> findByUser(User user);
+    Assignment findByIdAndUser(Long id, User user);
+    Optional<Assignment> findById(Long assignmentId);
 }

7c68d92986c4a7d210d6041eb0a3ff9e30c6a757
Harith A Kareem <harithakareem72@gmail.com>
2025-03-17 08:43:45 +0000

    fix passwordencoder

--- src/main/java/com/hakareem/Review/util/CustomPasswordEncoder.java
+++ src/main/java/com/hakareem/Review/util/CustomPasswordEncoder.java
@@ -1,5 +1,6 @@
 package com.hakareem.Review.util;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -7,10 +8,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomPasswordEncoder {
     private PasswordEncoder passwordEncoder;
+
     public CustomPasswordEncoder () {
         this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
+    @Bean
     public PasswordEncoder getPasswordEncoder() {
         return passwordEncoder;
     }
